e9ddce4a9dd542e73866a56365bfe9a2c14438e2
shim1998 <98shim@gmail.com>
2019-06-06 16:20:02 +0530

    Initial attempt at reading dataset

--- load.py
+++ load.py
@@ -1,1 +1,18 @@
 import cv2
+import re
+import os
+import numpy as np
+import pandas as pd
+
+def load_images_from_folder(folder):
+    images = []
+    for filename in os.listdir(folder):
+        img = cv2.imread(os.path.join(folder,filename))
+        if img is not None:
+            images.append(img)
+    return images
+
+hindi_letters=['ka','kha','ga','gha','kna','cha','chha','ja','jha','yna','taa','thaa','daa','dhaa','adna','ta','tha','da','na','pa','pha','ba','bha','ma','yaw','ra','la','va','sha','petchirayakha','patalosaw','ha','chaya','tra','gya']
+path='dataset/test/character_'
+for i in range(0,36):
+    load_images_from_folder(path+str(i+1))    
